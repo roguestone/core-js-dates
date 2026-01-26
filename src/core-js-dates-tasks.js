02f84c1ec065145c1c4c16883b0a8629398243bd
roguestone <roguestoneinweb@gmail.com>
2026-01-26 13:42:41 +0300

    feat: implement getQuarter

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -298,8 +298,19 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  const month = date.getMonth() + 1;
+
+  switch (true) {
+    case month <= 3:
+      return 1;
+    case month <= 6:
+      return 2;
+    case month <= 9:
+      return 3;
+    default:
+      return 4;
+  }
 }
 
 /**
